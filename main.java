5a858fa728227bcfbc25f51c62b1394f057d72ba
white <white.privat@gmail.com>
2025-04-14 21:01:25 +0200

    added ArrayList for months with 31 days (B1)

--- main.java
+++ main.java
@@ -1,3 +1,4 @@
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.IntSummaryStatistics;
 import java.util.LinkedList;
@@ -28,6 +29,16 @@ public class main {
 
         germanyBadenWuerttemberg.add(new Day(false, 26, 12, currentYear));
         ArrayList<Integer> germanyBadenWürtembergDayOfYearValues = Day.generateDayOfYearValues(germanyBadenWuerttemberg);
+
+        ArrayList<Integer> MonthsWith31Days = new ArrayList<>();
+        MonthsWith31Days.add(1);
+        MonthsWith31Days.add(3);
+        MonthsWith31Days.add(5);
+        MonthsWith31Days.add(7);
+        MonthsWith31Days.add(10);
+        MonthsWith31Days.add(12);
+
+
         // user input
         /*
          * Scanner scanner = new Scanner(System.in);
@@ -41,7 +52,7 @@ public class main {
 
         // create linkedList for every day in the year
         int currentMonth = 1;
-        int currentDay = 1;
+        int currentDay = 0;
         int startDayOfYear = 0;
         int daysInTheYear = 365;
         if (isLeapYear)
@@ -50,11 +61,14 @@ public class main {
         //time complexity for algorithm O=n^1
         for (int i = (startDayOfYear - 1); i < daysInTheYear; i++) {
             Day day = null;
-            if (currentDay == 31 && (currentMonth == 1 || currentMonth == 3 || currentMonth == 5 || currentMonth == 7
-                    || currentMonth == 10 || currentMonth == 12)) { //check for months with 31 days
+            if(currentDay == 31) {
+                System.out.println("INFO -> month with 31 days"+MonthsWith31Days.contains(currentMonth));
+            }
+            if (currentDay == 31 && MonthsWith31Days.contains(currentMonth)) { //check for months with 31 days
+                System.out.println("INFO -> new Month, current Mothn "+currentMonth+"current Day of the year "+i+"");
                 currentMonth++;
                 currentDay = 1;
-            } else if (currentDay == 30) { //check for months with 30 days
+            } else if (currentDay == 30 && !MonthsWith31Days.contains(currentMonth)) { //check for months with 30 days
                 currentMonth++;
                 currentDay = 1;
             } else if (!isLeapYear && currentDay == 28 && currentMonth == 2) { //check for februare
@@ -71,6 +85,7 @@ public class main {
                 day = new Day(false, currentDay, currentMonth, currentYear);
             }
             else if (germanyBadenWürtembergDayOfYearValues.contains(i)) {
+                System.out.println("INFO -> NEW HOLIDAY");
                 day = new Day(false,currentDay, currentMonth, currentYear);
             }
             else {
