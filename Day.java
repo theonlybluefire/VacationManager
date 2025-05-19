d161eee2cbe58ca63a4a170a218827c287d29316
white <white.privat@gmail.com>
2025-05-19 20:02:49 +0200

    resolved b2, doing t1

--- Day.java
+++ Day.java
@@ -10,10 +10,16 @@ public class Day {
     private int month;
     private int day;
     private int year;
+    final public int weekday;
     final public LocalDate date;
     final public int dayOfYear;
+    final public String weekdayWritten;
+    
+    public void setWorkday(boolean workday) {
+        this.workday = workday;
+    }
 
-    public Day(boolean isWorkday,int day, int month, int year) {
+    public Day(boolean isWorkday,int day, int month, int year, int START_WEEKDAY) {
         LocalDate date = LocalDate.of(year, month, day);
         this.workday = isWorkday;
         this.month = month;
@@ -21,6 +27,8 @@ public class Day {
         this.year = year;
         this.date = date;
         this.dayOfYear = date.getDayOfYear();
+        this.weekday = (START_WEEKDAY  + this.dayOfYear) % 7;
+        this.weekdayWritten = getWeekdayWritten(weekday);
     }
 
     public void setBooked(boolean booked) {
@@ -35,6 +43,27 @@ public class Day {
         }
 
         return dayOfYearArray;
+    }
+
+    static private String getWeekdayWritten(int weekday) {
+        switch (weekday) {
+            case 0:
+                return "Sunday";
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+            default:
+                return null;
+        }
     }
 
     
--- main.java
+++ main.java
@@ -9,14 +9,19 @@ public class main {
         // variables
         int leftoverVacationDays;
         int currentYear = 2025;
-        boolean isLeapYear = false;
+        final boolean IS_LEAP_YEAR = false;
         LinkedList<Day> haveToBookDays = new LinkedList<>();
-        LinkedList<Day> days = new LinkedList<>();
-
+        LinkedList<Day> days = new LinkedList<>();int currentMonth = 1;
+        int currentDay = 0;
+        int startDayOfYear = 0;
+        int daysInTheYear = 365;
+        final int START_WEEKDAY = 2;
+        if (IS_LEAP_YEAR)
+            daysInTheYear = 366;
         // holiday presets
         ArrayList<Day> germanyBadenWuerttemberg = new ArrayList<>();
-        germanyBadenWuerttemberg.add(new Day(false, 6, 1, currentYear));
-        germanyBadenWuerttemberg.add(new Day(false, 18, 4, currentYear));
+        germanyBadenWuerttemberg.add(new Day(false, 6, 1, currentYear, START_WEEKDAY));
+        germanyBadenWuerttemberg.add(new Day(false, 18, 4, currentYear, START_WEEKDAY));
         germanyBadenWuerttemberg.add(new Day(false, 21, 4, currentYear));
         germanyBadenWuerttemberg.add(new Day(false, 1, 5, currentYear));
         germanyBadenWuerttemberg.add(new Day(false, 29, 5, currentYear));
@@ -35,6 +40,7 @@ public class main {
         MonthsWith31Days.add(3);
         MonthsWith31Days.add(5);
         MonthsWith31Days.add(7);
+        MonthsWith31Days.add(8);
         MonthsWith31Days.add(10);
         MonthsWith31Days.add(12);
 
@@ -51,45 +57,39 @@ public class main {
          */
 
         // create linkedList for every day in the year
-        int currentMonth = 1;
-        int currentDay = 0;
-        int startDayOfYear = 0;
-        int daysInTheYear = 365;
-        if (isLeapYear)
-            daysInTheYear = 366;
 
-        //time complexity for algorithm O=n^1
-        for (int i = (startDayOfYear - 1); i < daysInTheYear; i++) {
-            Day day = null;
+        
+        
+
+        //create a list of all days in the year
+        for (int i = (startDayOfYear - 1); i < (daysInTheYear-1); i++) {
+            Day day = null; //create new day object
             if(currentDay == 31) {
                 System.out.println("INFO -> month with 31 days"+MonthsWith31Days.contains(currentMonth));
             }
-            if (currentDay == 31 && MonthsWith31Days.contains(currentMonth)) { //check for months with 31 days
+            if (currentDay == 31 && MonthsWith31Days.contains(currentMonth)) { //31 day month
                 System.out.println("INFO -> new Month, current Mothn "+currentMonth+"current Day of the year "+i+"");
                 currentMonth++;
                 currentDay = 1;
-            } else if (currentDay == 30 && !MonthsWith31Days.contains(currentMonth)) { //check for months with 30 days
+            } else if (currentDay == 30 && !MonthsWith31Days.contains(currentMonth)) { //30 day month
                 currentMonth++;
                 currentDay = 1;
-            } else if (!isLeapYear && currentDay == 28 && currentMonth == 2) { //check for februare
+            } else if (!IS_LEAP_YEAR && currentDay == 28 && currentMonth == 2) { // february and not leap year
                 currentMonth++;
                 currentDay=1;
-            } else if (isLeapYear && currentDay == 29 && currentMonth == 2 ) {
+            } else if (IS_LEAP_YEAR && currentDay == 29 && currentMonth == 2 ) { // february and leap year
                 currentMonth++;
                 currentDay=1;
-            } else {
+            } else { // normal day
                 currentDay++;
             }
 
-            if (i % 7 == 5 || i % 7 == 6) { // check for saturday or sunday
-                day = new Day(false, currentDay, currentMonth, currentYear);
+            day = new Day(false, currentDay, currentMonth, currentYear, START_WEEKDAY);
+            if (day.weekday == 7 || day.weekday==6) { // check for saturday or sunday
+                day.setWorkday(false);
             }
             else if (germanyBadenWürtembergDayOfYearValues.contains(i)) {
-                System.out.println("INFO -> NEW HOLIDAY");
-                day = new Day(false,currentDay, currentMonth, currentYear);
-            }
-            else {
-                day = new Day(true, currentDay, currentMonth, currentYear);
+                day.setWorkday(false);
             }
 
             days.add(day);
